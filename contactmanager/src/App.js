36dc645b41a31e11f4d78c3ec192b99f6869f567
Deepak <deepak25mb@gmail.com>
2019-02-27 06:46:49 +0530

    Setup Contact Manager

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
-import Header from "./Components/Header";
-import Contacts from "./Components/Contacts";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contacts />
-        </div>
+        <h1>The App component</h1>
       </div>
     );
   }
