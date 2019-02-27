b43447eef82819441d9dfe58e31c430ed8b059b6
Deepak <deepak25mb@gmail.com>
2019-02-27 07:21:58 +0530

    Completed Chapter 3 Unit 2

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>The App component</h1>
+        <h1>The App component</h1>
       </div>
     );
   }
