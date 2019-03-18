bd3fe38f44b948bc228a0f30ded57c55d05ce470
Deepak <deepak25mb@gmail.com>
2019-03-18 12:26:26 +0530

    Updates

---
+++ precache-manifest.c00a0dfd10f72e20d636a20112889ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c12872b10392c4836535",
+    "url": "/react-course/contactmanager/static/js/runtime~main.bc6bc823.js"
+  },
+  {
+    "revision": "73d2e060ef87bdc48846",
+    "url": "/react-course/contactmanager/static/js/main.7f861ed2.chunk.js"
+  },
+  {
+    "revision": "4aaad43ea7564c3ce850",
+    "url": "/react-course/contactmanager/static/js/2.72a15045.chunk.js"
+  },
+  {
+    "revision": "73d2e060ef87bdc48846",
+    "url": "/react-course/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4aaad43ea7564c3ce850",
+    "url": "/react-course/contactmanager/static/css/2.69fb07a3.chunk.css"
+  },
+  {
+    "revision": "b8b0ea45d133cbe600204ce9eddadb42",
+    "url": "/react-course/contactmanager/index.html"
+  }
+];
